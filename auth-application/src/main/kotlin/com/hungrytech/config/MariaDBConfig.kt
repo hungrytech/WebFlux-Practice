6a4fef03c12a5777c2879ba872d06754d2724ed7
hungrytech <xorals9448@gmail.com>
2023-03-26 22:35:45 +0900

    refactor: 표현식으로 변경

--- auth-application/src/main/kotlin/com/hungrytech/config/MariaDBConfig.kt
+++ auth-application/src/main/kotlin/com/hungrytech/config/MariaDBConfig.kt
@@ -8,30 +8,26 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.r2dbc.connection.R2dbcTransactionManager
-import org.springframework.transaction.ReactiveTransactionManager
 
 
 @Configuration
 class MariaDBConfig {
 
     @Bean
-    fun mariaDBConnectionFactory(r2dbcRepositories: R2DBCProperties): ConnectionFactory {
-        val conf: MariadbConnectionConfiguration = MariadbConnectionConfiguration.builder()
-            .host(r2dbcRepositories.host)
-            .port(r2dbcRepositories.port)
-            .username(r2dbcRepositories.username)
-            .password(r2dbcRepositories.password)
-            .database(r2dbcRepositories.database)
-            .allowPublicKeyRetrieval(true)
-            .build()
-
-
-        return MariadbConnectionFactory(conf)
-    }
+    fun mariaDBConnectionFactory(r2dbcRepositories: R2DBCProperties): ConnectionFactory =
+        MariadbConnectionFactory(
+            MariadbConnectionConfiguration.builder()
+                .host(r2dbcRepositories.host)
+                .port(r2dbcRepositories.port)
+                .username(r2dbcRepositories.username)
+                .password(r2dbcRepositories.password)
+                .database(r2dbcRepositories.database)
+                .allowPublicKeyRetrieval(true)
+                .build()
+        )
 
     @Bean
     @Primary
-    fun reactiveTransactionManager(@Qualifier("mariaDBConnectionFactory") connectionFactory: ConnectionFactory): ReactiveTransactionManager {
-        return R2dbcTransactionManager(connectionFactory)
-    }
+    fun reactiveTransactionManager(@Qualifier("mariaDBConnectionFactory") connectionFactory: ConnectionFactory) =
+        R2dbcTransactionManager(connectionFactory)
 }
