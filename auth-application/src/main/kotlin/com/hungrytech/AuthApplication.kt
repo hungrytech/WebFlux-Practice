0e154a89f3aafe44795a2fcd883c3003b57b26d6
hungrytech <xorals9448@gmail.com>
2023-03-26 22:40:22 +0900

    refactor: 사용하지 않은 주석 제거

--- auth-application/src/main/kotlin/com/hungrytech/AuthApplication.kt
+++ auth-application/src/main/kotlin/com/hungrytech/AuthApplication.kt
@@ -3,11 +3,9 @@ package com.hungrytech
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-//@EnableR2dbcAuditing
 class AuthApplication
 
 fun main(args: Array<String>) {
